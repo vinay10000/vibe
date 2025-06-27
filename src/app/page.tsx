c1171c37240eb904bfbe809f336d88950dce3f65
vinay <mhvinay5@gmail.com>
2025-06-27 16:58:57 +0530

    Update dependencies and db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="font-bold text-4xl p-4">Hello World</h1>
-      <Button variant="destructive" className="hover:bg-red-500 cursor-pointer">Click me</Button>
-    </div>
+    <h1>Hello World</h1>
   )
 }
