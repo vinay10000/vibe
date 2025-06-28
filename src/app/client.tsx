5ee56e8e75c6f0ca089e1a57e722e825912337d4
vinay <mhvinay5@gmail.com>
2025-06-28 14:46:13 +0530

    trpc setup

---
+++ src/app/client.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const Client = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.createAI.queryOptions({ text: "vinay PREFETCH" })
+  );
+  return <div>{JSON.stringify(data)}</div>;
+};
