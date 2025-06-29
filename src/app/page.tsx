0915b556a59cec8dc59cc0b4b611ca4a957022cb
vinay <mhvinay5@gmail.com>
2025-06-29 18:29:48 +0530

    AI JOBS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,35 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./client";
-import { Suspense } from "react";
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.createAI.queryOptions({ text: "vinay PREFETCH" })
-  );
-  const data = await queryClient.getQueryData(["createAI"]);
+"use client"
+import {useMutation } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+export default function Home() {
+  const [value, setValue] = useState("")
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("OpenAI background job invoked")
+    }
+  }))
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="max-w-md mx-auto mt-8 p-6 space-y-4">
+      <h1 className="text-2xl font-bold text-center">AI Summarizer</h1>
+      <Input 
+        value={value} 
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Enter text to summarize..."
+      />
+      <div className="space-y-2">
+        <Button 
+          onClick={() => invoke.mutate({value: value})} 
+          className="w-full"
+          disabled={!value.trim() || invoke.isPending}
+        >
+          {invoke.isPending ? "Processing..." : "Summarize with OpenAI"}
+        </Button>
+      </div>
+    </div>
   );
 }
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
 import { inngest } from "./client";
+import { createAgent, gemini } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system:
+        "You are a Next.js expert who writes clear, concise, and simple explanations for developers",
+      model: gemini({ model: "gemini-2.5-pro" }),
+    });
+    const { output } = await codeAgent.run(`${event.data.value}`);
+    console.log(output);
+    return { output };
+  }
 );
